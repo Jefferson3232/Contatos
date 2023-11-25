eb2cf29b8e75c9f3ef56f1cf76612f98becb289e
joao lucas <luccas332@gmail.com>
2023-11-25 16:43:36 -0300

    feat: add config seeder

--- database/seeders/ContactSeeder.php
+++ database/seeders/ContactSeeder.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Database\Seeders;
 
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class ContactSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        // Only run seeder select User
+        // Contact::factory(5)
+        // ->create([ 'user_id' =>  User::factory()]);
     }
 }
